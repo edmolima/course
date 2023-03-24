18ce434a38120a18ba0d981cbd9503e9eda4d915
Edmo Lima <edmo.lima@onefootball.com>
2023-03-24 12:45:38 +0100

    [New]: should replace buzz whenever there is a number that is divisible by 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,9 +2,12 @@ export const fizzbuzz = (number: number): string[] => {
   let result: string[] = [];
   for (let i = 1; i <= number; i++) {
     const isDivisibleBy3 = i % 3 === 0;
+    const isDivisibleBy5 = i % 5 === 0;
     
     if (isDivisibleBy3) {
       result.push('Fizz');
+    } else if (isDivisibleBy5) {
+      result.push('Buzz');
     } else {
       result.push(i.toString());
     }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -11,4 +11,13 @@ describe('FizzBuzz', () => {
 
     expect(actual).toEqual(expectedValue);
   });
+  test("should replace buzz whenever there is a number that is divisible by 5", () => {
+    const given = 5;
+
+    const expectedValue = ["1", "2", "Fizz", "4", "Buzz"];
+
+    const actual = fizzbuzz(given);
+
+    expect(actual).toEqual(expectedValue);
+  });
 });
